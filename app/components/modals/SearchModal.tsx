0e00e114800690c24493f9b7c1084014bbaa0f17
Zachary Short <zach.short@fantomworks.com>
2024-03-04 11:59:40 -0500

    search for produce now has second modal to type in product

--- app/components/modals/SearchModal.tsx
+++ app/components/modals/SearchModal.tsx
@@ -4,15 +4,13 @@
 import qs from "query-string";
 import dynamic from "next/dynamic";
 import { useCallback, useMemo, useState } from "react";
-import { Range } from "react-date-range";
-import { formatISO } from "date-fns";
 import { useRouter, useSearchParams } from "next/navigation";
-
 import useSearchModal from "@/app/hooks/useSearchModal";
-
+import Input from "../inputs/Input"; // Generic input component
 import Modal from "./Modal";
 import CountrySelect, { CountrySelectValue } from "../inputs/CountrySelect";
 import Heading from "../Heading";
+import { FieldValues, useForm } from "react-hook-form";
 
 // Enum defining the steps of the search process
 enum STEPS {
@@ -23,9 +21,20 @@ enum STEPS {
 // SearchModal component
 const SearchModal = () => {
   // Hooks for managing state and navigation
+  const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
   const searchModal = useSearchModal();
   const params = useSearchParams();
+
+  const {
+    register,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    // Default values for form fields
+    defaultValues: {
+      title: "",
+    },
+  });
 
   // State variables for managing search parameters
   const [step, setStep] = useState(STEPS.LOCATION);
@@ -49,7 +58,7 @@ const SearchModal = () => {
 
   // Function to handle form submission
   const onSubmit = useCallback(async () => {
-    if (step !== STEPS.LOCATION) {
+    if (step !== STEPS.ITEM) {
       return onNext();
     }
 
@@ -88,7 +97,7 @@ const SearchModal = () => {
 
   // Determine the label for the secondary action button
   const secondaryActionLabel = useMemo(() => {
-    if (step === STEPS.ITEM) {
+    if (step === STEPS.LOCATION) {
       return undefined;
     }
 
@@ -108,6 +117,27 @@ const SearchModal = () => {
     </div>
   );
 
+  if (step === STEPS.ITEM) {
+    // Form fields for specifying location
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="What are you looking for?"
+          subtitle="Search for produce & self sufficieny items"
+        />
+        <div>
+          <Input
+            id="title"
+            label="Product"
+            disabled={isLoading}
+            register={register}
+            errors={errors}
+            required
+          />
+        </div>
+      </div>
+    );
+  }
   // Update bodyContent based on the current step
 
   // Render the SearchModal component
