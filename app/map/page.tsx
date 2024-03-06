1629d5ddc04636c0a9ce6676667d77a0a901c8c9
Zachary Short <zach.short@fantomworks.com>
2024-03-06 11:34:09 -0500

    google map

---
+++ app/map/page.tsx
@@ -0,0 +1,17 @@
+import { Button } from "../components/ui/button";
+import { MapTest } from "../components/map/MapTest";
+
+const Home = () => {
+  return (
+    <div>
+      <main>
+        <div>
+          <h1>Map Test Page</h1>
+          <MapTest />
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
