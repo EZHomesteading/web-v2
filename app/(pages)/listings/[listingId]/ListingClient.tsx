5dbfdc1d835db8220f95ffe6c8c375c30955e18c
Zachary Short <zach.short@fantomworks.com>
2024-03-12 09:56:01 -0400

    listing page changes

--- app/(pages)/listings/[listingId]/ListingClient.tsx
+++ app/(pages)/listings/[listingId]/ListingClient.tsx
@@ -82,50 +82,37 @@ const ListingClient: React.FC<ListingClientProps> = ({
         className="
           max-w-screen-lg 
           mx-auto
+          mt-8
         "
       >
-        <div className="flex flex-col gap-6">
-          <ListingHead
-            title={listing.title}
-            city={listing.city}
-            state={listing.state}
-            imageSrc={listing.imageSrc}
-            id={listing.id}
-            currentUser={currentUser}
-          />
-          <div
-            className="
-              grid 
-              grid-cols-1 
-              md:grid-cols-7 
-              md:gap-10 
-              mt-6
-            "
-          >
+        <div className="flex flex-wrap">
+          <div className="w-full lg:w-2/3">
+            <ListingHead
+              title={listing.title}
+              city={listing.city}
+              state={listing.state}
+              imageSrc={listing.imageSrc}
+              id={listing.id}
+              currentUser={currentUser}
+            />
             <ListingInfo
               user={listing.user}
               description={listing.description}
             />
-            <div
-              className="
-                order-first 
-                mb-10 
-                md:order-last 
-                md:col-span-3
-              "
-            >
-              <ListingReservation
-                product={adjustedListing}
-                onSubmit={onCreatePurchase}
-                disabled={isLoading}
+          </div>
+          <div className="w-full lg:w-1/3 px-4">
+            <ListingReservation
+              product={adjustedListing}
+              onSubmit={onCreatePurchase}
+              disabled={isLoading}
+            />
+            <div className="mt-8">
+              <ListingMap
+                street={listing.street}
+                city={listing.city}
+                state={listing.state}
               />
             </div>
-            <p>{shelfLifeDisplay}</p>
-            <ListingMap
-              street={listing.street}
-              city={listing.city}
-              state={listing.state}
-            />
           </div>
         </div>
       </div>
