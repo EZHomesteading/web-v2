593d81fb33af32ca3d713f20c94634e4a690cc38
Zachary Short <zach.short@fantomworks.com>
2024-03-19 11:27:30 -0400

    checks on api search if query, location, radius are empty and handle cases for all

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -8,35 +8,46 @@ export async function GET(request: Request) {
   const query = searchParams.get("query") || "";
   const lat = searchParams.get("lat") || "";
   const lng = searchParams.get("lng") || "";
-  const radius = searchParams.get("radius") || "20000";
+  const radius = searchParams.get("radius") || "30000";
 
   try {
-    const listings = await prisma.listing.findMany();
-
-    const nearbyListings = listings.filter((listing) => {
-      const listingLocation = listing.location as {
-        coordinates: [number, number];
-      };
-      const listingCoordinates = {
-        latitude: listingLocation.coordinates[1],
-        longitude: listingLocation.coordinates[0],
-      };
-      const userLocation = {
-        latitude: parseFloat(lat),
-        longitude: parseFloat(lng),
-      };
-      const distance = haversine(listingCoordinates, userLocation);
-      return distance <= parseFloat(radius);
+    const listings = await prisma.listing.findMany({
+      include: {
+        user: true,
+      },
     });
 
-    const fuseOptions = {
-      keys: ["title", "category", "subCategory", "description"],
-    };
-    console.log("query:", query);
-    const fuse = new Fuse(nearbyListings, fuseOptions);
-    console.log("fuse", fuse);
-    const results = fuse.search(query);
-    return NextResponse.json(results);
+    let filteredListings = listings;
+
+    if (lat !== "" && lng !== "") {
+      filteredListings = listings.filter((listing) => {
+        const listingLocation = listing.location as {
+          coordinates: [number, number];
+        };
+        const listingCoordinates = {
+          latitude: listingLocation.coordinates[1],
+          longitude: listingLocation.coordinates[0],
+        };
+        const userLocation = {
+          latitude: parseFloat(lat),
+          longitude: parseFloat(lng),
+        };
+        const distance = haversine(listingCoordinates, userLocation);
+        return distance <= parseFloat(radius);
+      });
+    }
+
+    if (query !== "") {
+      const fuseOptions = {
+        keys: ["user.name", "title", "category", "subCategory", "description"],
+        threshold: 0.3,
+      };
+      console.log("query:", query);
+      const fuse = new Fuse(filteredListings, fuseOptions);
+      console.log("fuse", fuse);
+      const results = fuse.search(query);
+      return NextResponse.json(results);
+    } else return NextResponse.json(filteredListings);
   } catch (error) {
     return NextResponse.json(
       { error: "An error occurred while searching listings" },
