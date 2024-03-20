5d02c1dccc133ec954f95eb74d24549d0324bcb9
Zachary Short <zach.short@fantomworks.com>
2024-03-20 13:56:09 -0400

    consolidated co-op hours selector to one component with props

--- app/(pages)/co-op-hours/page.tsx
+++ app/(pages)/co-op-hours/page.tsx
@@ -1,34 +1,49 @@
 "use client";
-import ComboboxPopover from "@/app/components/ui/co-op-hours-open";
-import ComboboxPopoverr from "@/app/components/ui/co-op-hours-closed";
+import HourPicker from "./co-op-hour-select";
+import { CoOpHours, initialCoOpHours } from "./co-op-hours";
 import { useState } from "react";
+
 const CoOpHoursPage = () => {
+  const [coOpHours, setCoOpHours] = useState<CoOpHours[]>(initialCoOpHours);
+
+  const handleHourChange = (
+    day: string,
+    field: "open" | "close",
+    value: string
+  ) => {
+    setCoOpHours((prevHours) =>
+      prevHours.map((hours) => {
+        if (hours.day === day) {
+          return { ...hours, [field]: value };
+        }
+        return hours;
+      })
+    );
+  };
+
   return (
     <>
-      <div className="flex flex-col justify-center items-center mt-10">
-        <label>Monday</label>
-        <ComboboxPopover
-        // onChange={(value) => {setValue("mondayOpen", value))}}
-        />
-        <ComboboxPopoverr />
-        <label>Tuesday</label>
-        <ComboboxPopover />
-        <ComboboxPopoverr />
-        <label>Wednesday</label>
-        <ComboboxPopover />
-        <ComboboxPopoverr />
-        <label>Thursday</label>
-        <ComboboxPopover />
-        <ComboboxPopoverr />
-        <label>Friday</label>
-        <ComboboxPopover />
-        <ComboboxPopoverr />
-        <label>Saturday</label>
-        <ComboboxPopover />
-        <ComboboxPopoverr />
-        <label>Sunday</label>
-        <ComboboxPopover />
-        <ComboboxPopoverr />
+      <div className="flex flex-col justify-center items-center gap-4 mt-10">
+        <div className="space-y-4">
+          {coOpHours.map((hours) => (
+            <div key={hours.day} className="flex flex-col items-end space-y-1">
+              <HourPicker
+                sideLabel={`${hours.day}`}
+                buttonLabel="Open"
+                value={hours.open}
+                onChange={(value) => handleHourChange(hours.day, "open", value)}
+              />
+              <HourPicker
+                sideLabel=""
+                buttonLabel="Close"
+                value={hours.close}
+                onChange={(value) =>
+                  handleHourChange(hours.day, "close", value)
+                }
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
