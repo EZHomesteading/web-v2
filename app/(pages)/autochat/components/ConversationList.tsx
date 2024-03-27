bac4189bc482d575f1e577ebdf55ea483ec6cfee
macrowil <macrow.co@gmail.com>
2024-03-27 15:25:31 -0600

    building web-push api integration, EOD push, stopped before building saving user settings

--- app/(pages)/autochat/components/ConversationList.tsx
+++ app/(pages)/autochat/components/ConversationList.tsx
@@ -13,6 +13,8 @@ import { pusherClient } from "@/app/libs/pusher";
 import GroupChatModal from "@/app/components/modals/chatmodals/GroupChatModal";
 import ConversationBox from "./ConversationBox";
 import { FullConversationType } from "@/app/types";
+import { registerServiceWorker } from "@/app/hooks/serviceWorker";
+import SubToggle from "./notificationButton";
 
 interface ConversationListProps {
   initialItems: FullConversationType[];
@@ -37,10 +39,20 @@ const ConversationList: React.FC<ConversationListProps> = ({
   }, [session.data?.user?.email]);
 
   useEffect(() => {
+    async function setUpServiceWorker() {
+      try {
+        await registerServiceWorker();
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    setUpServiceWorker();
+  });
+
+  useEffect(() => {
     if (!pusherKey) {
       return;
     }
-
     pusherClient.subscribe(pusherKey);
 
     const updateHandler = (conversation: FullConversationType) => {
@@ -91,7 +103,7 @@ const ConversationList: React.FC<ConversationListProps> = ({
           className={clsx(
             `
         fixed
-        inset-y-0
+        inset-y-20
         pb-20
         lg:pb-0
       
@@ -122,8 +134,22 @@ const ConversationList: React.FC<ConversationListProps> = ({
               "
               >
                 <MdOutlineGroupAdd size={20} />
+              </div>{" "}
+              <div
+                className="
+                rounded-full 
+                p-2 
+                bg-gray-100 
+                text-gray-600 
+                cursor-pointer 
+                hover:opacity-75 
+                transition
+              "
+              >
+                <SubToggle />
               </div>
             </div>
+
             {items.map((item) => (
               <ConversationBox
                 key={item.id}
