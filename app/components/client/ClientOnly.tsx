dac05756b3471d94dc937862f149451fdd669ac0
Zachary Short <zach.short@fantomworks.com>
2024-03-27 12:56:23 -0400

    deleted unused client files in components

--- app/components/client/ClientOnly.tsx
+++ app/components/client/ClientOnly.tsx
@@ -2,30 +2,20 @@
 
 import React, { useState, useEffect } from "react";
 
-// Define props interface for the ClientOnly component
 interface ClientOnlyProps {
-  children: React.ReactNode; // Child components to be rendered conditionally
+  children: React.ReactNode;
 }
 
-// ClientOnly component
-const ClientOnly: React.FC<ClientOnlyProps> = ({
-  children, // Destructure children from props
-}) => {
-  // State to track whether the component has mounted
+const ClientOnly: React.FC<ClientOnlyProps> = ({ children }) => {
   const [hasMounted, setHasMounted] = useState(false);
 
-  // Effect hook to set hasMounted to true when component mounts
   useEffect(() => {
     setHasMounted(true);
   }, []);
 
-  // Render children only after the component has mounted
   if (!hasMounted) return null;
 
-  return (
-    // Return the children components
-    <>{children}</>
-  );
+  return <>{children}</>;
 };
 
 export default ClientOnly;
