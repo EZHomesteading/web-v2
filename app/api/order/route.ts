7e0b21d6b89dbbbae04f88ee9537d4bdf004fec7
Zachary Short <zach.short@fantomworks.com>
2024-04-05 23:18:14 -0400

    converted order create api route back to ts and imported necessary packages

--- app/api/order/route.ts
+++ app/api/order/route.ts
@@ -1,4 +1,9 @@
-export async function POST(request) {
+import { NextResponse, NextRequest } from "next/server";
+import { currentUser } from "@/lib/auth";
+import prisma from "@/lib/prisma";
+import getListingById from "@/actions/getListingById";
+
+export async function POST(request: NextRequest) {
   const user = await currentUser();
   if (!user) {
     return NextResponse.error();
