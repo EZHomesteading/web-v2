88f03323148d3c17faca3c2f6678ad2ac857a443
Zachary Short <zach.short@fantomworks.com>
2024-04-26 13:38:46 -0400

    improved ui on delete listing warning

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -72,7 +72,7 @@ const ListingCard: React.FC<ListingCardProps> = ({
 
   return (
     <div className="col-span-1 cursor-pointer group">
-      <div className="flex flex-col gap-2 w-full">
+      <div className="flex flex-col w-full">
         <div
           onClick={() => router.push(`/listings/${data.id}`)}
           className="
@@ -121,39 +121,40 @@ const ListingCard: React.FC<ListingCardProps> = ({
               )}
             </div>
           </div>
-          <div className="flex justify-end"></div>
+          <div className="flex justify-end">
+            <AlertDialog>
+              <AlertDialogTrigger>
+                {onAction && actionLabel && (
+                  <Button className="bg-transparent shadow-none text-red-600 text-3xl hover:bg-transparent hover:scale-105 pt-0">
+                    <FaDeleteLeft />
+                  </Button>
+                )}
+              </AlertDialogTrigger>
+              <AlertDialogContent className="bg-black rounded-lg px-4 py-4 w-fit ">
+                <AlertDialogHeader className="text-3xl">
+                  Are you sure?
+                </AlertDialogHeader>
+                <AlertDialogDescription className="text-white pt-2">
+                  We cannot recover a listing after it has been deleted, this is
+                  irreversible.
+                </AlertDialogDescription>
+
+                <AlertDialogFooter className="flex items-center justify-start gap-x-5 pt-3">
+                  <AlertDialogAction
+                    className="shadow-none bg-red-600 text-3xl hover:bg-red-700 text-md"
+                    onClick={handleCancel}
+                  >
+                    Yes, I&apos;m sure
+                  </AlertDialogAction>
+                  <AlertDialogCancel className=" shadow-none bg-green-600 text-3xl hover:bg-green-700 text-md text-white border-none hover:text-white m-0">
+                    Nevermind
+                  </AlertDialogCancel>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+          </div>
         </div>
 
-        <AlertDialog>
-          <AlertDialogTrigger>
-            {onAction && actionLabel && (
-              <Button className="bg-transparent shadow-none text-red-600 text-3xl hover:bg-transparent hover:scale-105">
-                <FaDeleteLeft />
-              </Button>
-            )}
-          </AlertDialogTrigger>
-          <AlertDialogContent className="bg-black rounded-lg px-4 py-4 w-fit ">
-            <AlertDialogHeader className="text-3xl">
-              Are you sure?
-            </AlertDialogHeader>
-            <AlertDialogDescription className="text-white pt-2">
-              We cannot recover a listing after it has been deleted, this is
-              irreversible.
-            </AlertDialogDescription>
-
-            <AlertDialogFooter className="flex items-center justify-start gap-x-5 pt-3">
-              <AlertDialogAction
-                className="shadow-none bg-red-600 text-3xl hover:bg-red-700 text-md"
-                onClick={handleCancel}
-              >
-                Yes, I&apos;m sure
-              </AlertDialogAction>
-              <AlertDialogCancel className=" shadow-none bg-green-600 text-3xl hover:bg-green-700 text-md text-white border-none hover:text-white m-0">
-                Nevermind
-              </AlertDialogCancel>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialog>
         {onSecondAction && secondActionLabel && (
           <Button
             disabled={disabled}
