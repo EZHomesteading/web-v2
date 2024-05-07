72359ee612fb1221a0c74bf05e204886535bec45
macrowil <macrow.co@gmail.com>
2024-05-07 12:29:58 -0600

    fleshed out review sheet. now will accept a rating as stars and user text input, has a close button, and closes on submit. added reviews object to schema

--- app/components/listings/ListingInfo.tsx
+++ app/components/listings/ListingInfo.tsx
@@ -3,6 +3,7 @@
 import { UserInfo } from "@/next-auth";
 import { useRouter } from "next/navigation";
 import FollowButton from "../follow/followButton";
+import ReviewButton from "../ui/reviewButton";
 
 interface ListingInfoProps {
   user: UserInfo;
@@ -45,6 +46,7 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
             following={following}
             user={user}
           />
+          <ReviewButton />
         </div>
       </div>
     </div>
---
+++ app/components/ui/reviewButton.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { SheetCartC, SheetContentF } from "@/app/components/ui/review-sheet";
+import { SheetTrigger } from "@/app/components/ui/sheet";
+import { useState } from "react";
+
+import "react-datetime-picker/dist/DateTimePicker.css";
+
+interface StatusProps {}
+
+const ReviewButton = ({}: StatusProps) => {
+  return (
+    <SheetCartC>
+      <SheetTrigger className="border-[1px] px-2 py-2 rounded-lg shadow-lg">
+        Write a review
+      </SheetTrigger>
+      <SheetContentF
+        side="top"
+        className="border-none h-screen w-screen bg-transparent flex flex-col lg:flex-row justify-center lg:justify-evenly items-center"
+      ></SheetContentF>
+    </SheetCartC>
+  );
+};
+
+export default ReviewButton;
