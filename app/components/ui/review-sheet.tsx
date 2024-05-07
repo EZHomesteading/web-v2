5924bbd47776bcf06e26dcfc0a04f16d88b2a307
macrowil <macrow.co@gmail.com>
2024-05-07 15:07:40 -0600

    optimising review button

--- app/components/ui/review-sheet.tsx
+++ app/components/ui/review-sheet.tsx
@@ -144,58 +144,6 @@ const SheetContentF = React.forwardRef<
 });
 SheetContentF.displayName = SheetPrimitive.Content.displayName;
 
-const SheetHeaderC = ({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLDivElement>) => (
-  <div
-    className={cn(
-      "flex flex-col space-y-2 text-center sm:text-left",
-      className
-    )}
-    {...props}
-  />
-);
-SheetHeaderC.displayName = "SheetHeader";
-
-const SheetFooterC = ({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLDivElement>) => (
-  <div
-    className={cn(
-      "flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2",
-      className
-    )}
-    {...props}
-  />
-);
-SheetFooterC.displayName = "SheetFooter";
-
-const SheetTitle = React.forwardRef<
-  React.ElementRef<typeof SheetPrimitive.Title>,
-  React.ComponentPropsWithoutRef<typeof SheetPrimitive.Title>
->(({ className, ...props }, ref) => (
-  <SheetPrimitive.Title
-    ref={ref}
-    className={cn("text-lg font-semibold text-foreground", className)}
-    {...props}
-  />
-));
-SheetTitle.displayName = SheetPrimitive.Title.displayName;
-
-const SheetDescription = React.forwardRef<
-  React.ElementRef<typeof SheetPrimitive.Description>,
-  React.ComponentPropsWithoutRef<typeof SheetPrimitive.Description>
->(({ className, ...props }, ref) => (
-  <SheetPrimitive.Description
-    ref={ref}
-    className={cn("text-sm text-muted-foreground", className)}
-    {...props}
-  />
-));
-SheetDescription.displayName = SheetPrimitive.Description.displayName;
-
 export {
   SheetCartC,
   SheetPortalC,
@@ -203,8 +151,4 @@ export {
   SheetTriggerC,
   SheetCloseC,
   SheetContentF,
-  SheetHeaderC,
-  SheetFooterC,
-  SheetTitle,
-  SheetDescription,
 };
