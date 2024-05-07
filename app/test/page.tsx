8c7bad2f44742e2da6d95d2c913797f45ef23a66
Zachary Short <zach.short@fantomworks.com>
2024-05-06 20:18:07 -0400

    slowly losing sanity

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -2,9 +2,9 @@ import SubToggle from "../(pages)/chat/components/notificationButton";
 
 const Page = () => {
   return (
-    <>
+    <div className="min-h-screen justify-center items-center flex">
       <SubToggle />
-    </>
+    </div>
   );
 };
 
