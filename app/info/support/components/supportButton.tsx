60160c6f5063c4cadb5e378f6451ff3a318f3c6f
macrowil <macrow.co@gmail.com>
2024-05-14 12:11:23 -0600

    added support page. with basic start of Faq, and a place to display to users our contact information

---
+++ app/info/support/components/supportButton.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { SheetCartC, SheetContentS } from "@/app/components/ui/support-sheet";
+import { SheetTrigger } from "@/app/components/ui/sheet";
+
+import "react-datetime-picker/dist/DateTimePicker.css";
+
+interface StatusProps {
+  email: string;
+}
+
+const SupportButton = ({ email }: StatusProps) => {
+  return (
+    <SheetCartC>
+      <SheetTrigger>Contact Us</SheetTrigger>
+      <SheetContentS
+        side="top"
+        className="border-none h-screen w-screen bg-transparent flex flex-col lg:flex-row justify-center lg:justify-evenly items-center"
+        email={email}
+      ></SheetContentS>
+    </SheetCartC>
+  );
+};
+
+export default SupportButton;
