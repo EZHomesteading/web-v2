4e8bdc4cbe9584f56f27aef5a4e8a365130b6497
Zachary Short <zach.short@fantomworks.com>
2024-05-15 11:59:43 -0400

    test file for uploading an image to confirm delivery

--- app/chat/[conversationId]/components/MessageBox.tsx
+++ app/chat/[conversationId]/components/MessageBox.tsx
@@ -341,7 +341,7 @@ const MessageBox: React.FC<MessageBoxProps> = ({
           </div>
           <div className={message}>
             <ImageModal
-              isOpen={imageModalOpen}
+              isOpen={true}
               onClose={() => setImageModalOpen(false)}
             />
             {data.image ? (
