031f6ef0da8547cf1eab85ff37be4a030fcac8e8
Zachary Short <zach.short@fantomworks.com>
2024-05-22 10:32:51 -0400

    optimized the next/image in the listing card

--- app/(pages)/market/market-component.tsx
+++ app/(pages)/market/market-component.tsx
@@ -31,12 +31,13 @@ const Shop = ({
       <Container>
         {emptyState || (
           <div className="pt-2 md:pt-5 grid  grid-cols-1 sm:grid-cols-2  md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-            {listings.map((listing: any) => (
+            {listings.map((listing: any, index) => (
               <ListingCard
                 user={user}
                 key={listing.id}
                 data={listing}
                 storeUser={listing.user}
+                priority={index === 0}
               />
             ))}
           </div>
