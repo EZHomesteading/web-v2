1bc881051b5c89ab86a85d869972bbbd4587fc85
macrowil <macrow.co@gmail.com>
2024-07-15 11:05:40 -0600

    coops without locations are now directed to the store settings page if they click the create button with no location set. users can now press a single button to chaneg their primary, secondary and tertiary locations

--- app/api/useractions/update/route.ts
+++ app/api/useractions/update/route.ts
@@ -5,6 +5,7 @@ import prisma from "@/lib/prismadb";
 
 export async function POST(request: Request) {
   const body = await request.json();
+  console.log(body);
   const {
     firstName,
     name,
@@ -24,7 +25,7 @@ export async function POST(request: Request) {
     return NextResponse.error();
   }
   let updatedUser;
-  console.log(image);
+  //console.log(image);
   updatedUser = await prisma.user.update({
     where: { id: user.id },
     data: {
