31172aa6824245b2eea38bcedabb6ba0bf16e905
Zach <shortzach396@gmail.com>
2024-08-29 15:49:05 -0400

    removed isloading which prevents market items from appearing

--- app/(pages)/market/market-component.tsx
+++ app/(pages)/market/market-component.tsx
@@ -34,9 +34,9 @@ const Shop = ({
   pageNumbers,
   currentPage,
 }: ShopProps) => {
-  const router = useRouter();
-  const [isLoading, setIsLoading] = useState(true);
-  const [showPopup, setShowPopup] = useState(false);
+  // const router = useRouter();
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [showPopup, setShowPopup] = useState(false);
 
   // useEffect(() => {
   //   const getLocation = () => {
@@ -83,55 +83,55 @@ const Shop = ({
       <SessionStorageManager />
       <Container>
         {/* {showPopup && <LocationPermissionPopup />} */}
-        {isLoading ? (
+        {/* {isLoading ? (
           <div className="pt-2 md:pt-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
             {Array.from({ length: 12 }).map((_, index) => (
               <SkeletonCard key={index} />
             ))}
           </div>
         ) : (
+          <> */}
+        {emptyState || (
+          <div className="pt-2 md:pt-5 grid  grid-cols-1 sm:grid-cols-2  md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+            {listings.map((listing: FinalListing, index) => (
+              <ListingCard
+                user={user}
+                key={listing.id}
+                data={listing}
+                storeUser={listing.user as unknown as UserInfo}
+                priority={index === 0}
+              />
+            ))}
+          </div>
+        )}
+        {totalPages > 1 && (
           <>
-            {emptyState || (
-              <div className="pt-2 md:pt-5 grid  grid-cols-1 sm:grid-cols-2  md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-                {listings.map((listing: FinalListing, index) => (
-                  <ListingCard
-                    user={user}
-                    key={listing.id}
-                    data={listing}
-                    storeUser={listing.user as unknown as UserInfo}
-                    priority={index === 0}
-                  />
-                ))}
+            {isPageOutOfRange ? (
+              <></>
+            ) : (
+              <div className="flex justify-center items-end mt-16">
+                <div className="flex border-[1px] gap-4 rounded-[10px] border-light-green p-4">
+                  {/* builds cards dynamically based on data passed in */}
+                  {pageNumbers.map((pageNumber, index) => (
+                    <Link
+                      key={index}
+                      className={
+                        currentPage === pageNumber
+                          ? "bg-green-600 fw-bold px-2 rounded-md text-black"
+                          : "hover:bg-green-600 px-1 rounded-md"
+                      }
+                      href={`?page=${pageNumber}`}
+                    >
+                      {pageNumber}
+                    </Link>
+                  ))}
+                </div>
               </div>
-            )}
-            {totalPages > 1 && (
-              <>
-                {isPageOutOfRange ? (
-                  <></>
-                ) : (
-                  <div className="flex justify-center items-end mt-16">
-                    <div className="flex border-[1px] gap-4 rounded-[10px] border-light-green p-4">
-                      {/* builds cards dynamically based on data passed in */}
-                      {pageNumbers.map((pageNumber, index) => (
-                        <Link
-                          key={index}
-                          className={
-                            currentPage === pageNumber
-                              ? "bg-green-600 fw-bold px-2 rounded-md text-black"
-                              : "hover:bg-green-600 px-1 rounded-md"
-                          }
-                          href={`?page=${pageNumber}`}
-                        >
-                          {pageNumber}
-                        </Link>
-                      ))}
-                    </div>
-                  </div>
-                )}
-              </>
             )}
           </>
         )}
+        {/* </>
+        )} */}
       </Container>
     </ClientOnly>
   );
