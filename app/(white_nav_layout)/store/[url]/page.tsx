114637aad0afaae1f8c3651f306357d0f13210a6
zachmshort <zach.short@fantomworks.com>
2024-10-29 13:45:40 -0400

    info on routes

---
+++ app/(white_nav_layout)/store/[url]/page.tsx
@@ -0,0 +1,49 @@
+//server side layout for users dynamically generated stores.
+import dynamic from "next/dynamic";
+import EmptyState from "@/components/EmptyState";
+import ClientOnly from "@/components/client/ClientOnly";
+import { getFollows } from "@/actions/getFollow";
+import { StoreData, getUserStore } from "@/actions/getUser";
+import SessionStorageManager from "@/components/sessionStorageManager";
+import { getUserwithCart } from "@/actions/getUser";
+import { UserInfo } from "next-auth";
+
+interface StorePageProps {
+  params: {
+    url: string;
+  };
+}
+
+const DynamicStorePage = dynamic(
+  () => import("@/app/(white_nav_layout)/store/[url]/Store"),
+  {
+    ssr: true,
+  }
+);
+const StorePage = async ({ params }: StorePageProps) => {
+  const { url } = params;
+  const store = await getUserStore({ url: url });
+  const user = await getUserwithCart();
+  const following = await getFollows();
+  return (
+    <>
+      <ClientOnly>
+        <SessionStorageManager />
+      </ClientOnly>
+      <DynamicStorePage
+        store={store}
+        user={user as unknown as UserInfo}
+        following={following}
+        emptyState={
+          !store ? (
+            <ClientOnly>
+              <EmptyState showReset />
+            </ClientOnly>
+          ) : null
+        }
+      />
+    </>
+  );
+};
+
+export default StorePage;
