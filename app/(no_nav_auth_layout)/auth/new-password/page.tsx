0f7eb81ecaa8094ca65cbfa08ca3d1e9622e561f
zachmshort <zach.short@fantomworks.com>
2025-02-05 20:26:54 -0500

    deleting unused code

--- app/(no_nav_auth_layout)/auth/new-password/page.tsx
+++
@@ -1,8 +0,0 @@
-//server layout for reset password form
-import { NewPasswordForm } from "@/app/(no_nav_auth_layout)/auth/(components)/new-password-form";
-
-const NewPasswordPage = () => {
-  return <NewPasswordForm />;
-};
-
-export default NewPasswordPage;
