07630c726c9723eea8b1b18f8c06e6a20827e1ec
zachmshort <zach.short@fantomworks.com>
2025-02-15 18:13:28 -0500

    404

--- app/[...not_found]/page.tsx
+++ app/[...not_found]/page.tsx
@@ -1,38 +1,44 @@
-import { Outfit } from "next/font/google";
+import React from "react";
+import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
-const outfit = Outfit({
-  subsets: ["latin"],
-  display: "swap",
-});
-export default function NotFound() {
+import Image from "next/image";
+import { OutfitFont } from "@/components/fonts";
+
+const Page404 = () => {
   return (
     <div
-      className={`${outfit.className} flex flex-col h-[100vh] d1dbbf items-center justify-start pt-[15%]`}
+      className={`${OutfitFont.className} relative h-screen w-screen overflow-hidden`}
     >
-      <div className="text-center">
-        <h1 className="text-[7rem] font-bold">Oops</h1>
-        <h2 className="text-xl mb-8">404 - This page could not be found.</h2>
-        <div className="flex justify-center space-x-4">
-          <Link
-            href="/"
-            className="px-4 py-2 border w-[100px] text-white rounded"
-          >
-            Home
-          </Link>
-          <Link
-            href="/market"
-            className="px-4 w-[100px] py-2 border text-white rounded "
-          >
-            Market
-          </Link>{" "}
-          <Link
-            href="/chat"
-            className="px-4 py-2 border w-[100px] text-white rounded "
-          >
-            Chat
-          </Link>
-        </div>
+      <div className="absolute inset-0">
+        <Image
+          src="https://ruhn2hhuu8.ufs.sh/f/UE11RVNWlVtTVdG4FFjDhnba7pxgyuH9UBWz3VvNFdw8qMsQ"
+          alt="Organic farm background"
+          fill
+          className="object-cover w-full h-full"
+        />
+        <div className="absolute inset-0 bg-black/80" />
+      </div>
+
+      <div className="relative z-10 flex flex-col items-center justify-center h-full text-center px-4">
+        <h1 className="text-6xl font-bold text-green-400 mb-4">OH CROP!</h1>
+        <p className="text-2xl max-w-md text-white mb-8">
+          Looks like you got lost in the corn maze... or maybe we gave you bad
+          directions.
+        </p>
+        <Link
+          href={`/`}
+          className="flex items-center gap-2 px-6 py-3 bg-emerald-500 text-white rounded-full hover:bg-green-600 transition-colors"
+        >
+          <ArrowLeft size={20} />
+          Back to Homepage
+        </Link>
+        <p className="text-sm text-gray-300 absolute bottom-5 max-w-md px-4">
+          Is this our fault or yours? We don't like to point fingers, but its
+          probably ours. We're a small team, sorry.
+        </p>
       </div>
     </div>
   );
-}
+};
+
+export default Page404;
